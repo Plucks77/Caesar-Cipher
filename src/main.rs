72571128b7ff5bc6b4ea3096ecd4438c1c2c7743
plucks77 <pedro.leoti.pessoal@gmail.com>
2022-08-02 08:11:49 -0300

    decipher

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ fn main() {
     println!("Please provide your phrase");
     io::stdin().read_line(&mut read_phrase).unwrap();
 
-    let new_phrase = cipher(read_phrase, alphabet, offset.try_into().unwrap());
-    println!("Cipher: {:?}", new_phrase)
+    let new_phrase = cipher(read_phrase, alphabet.clone(), offset.try_into().unwrap());
+    println!("Cipher: {:?}", new_phrase);
+    let old_phrase = decipher(new_phrase, alphabet, offset.try_into().unwrap());
+    println!("Decipher: {:?}", old_phrase)
 }
 
 fn cipher(phrase: String, alphabet: Vec<char>, offset: usize) -> String {
@@ -25,6 +27,21 @@ fn cipher(phrase: String, alphabet: Vec<char>, offset: usize) -> String {
         } else {
             let index = alphabet.iter().position(|&r| r == c).unwrap();
             let at_index = index + offset;
+            let current = alphabet[at_index];
+            new_phrase.push(current)
+        }
+    }
+    new_phrase
+}
+
+fn decipher(cipher: String, alphabet: Vec<char>, offset: usize) -> String {
+    let mut new_phrase = String::new();
+    for c in cipher.trim().chars() {
+        if c == ' ' {
+            new_phrase.push(' ')
+        } else {
+            let index = alphabet.iter().position(|&r| r == c).unwrap();
+            let at_index = index - offset;
             let current = alphabet[at_index];
             new_phrase.push(current)
         }
