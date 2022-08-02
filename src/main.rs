6bd7b78c0a3fbcdaacc26244786ad3227f664520
plucks77 <pedro.leoti.pessoal@gmail.com>
2022-08-02 08:06:23 -0300

    first try

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main() {
+    let alphabet: Vec<char> = ('a'..='z').collect();
+
+    let mut read_offset = String::new();
+    let mut read_phrase = String::new();
+
+    println!("Please provide the offset");
+    io::stdin().read_line(&mut read_offset).unwrap();
+    let offset: i32 = read_offset.trim().parse().unwrap();
+
+    println!("Please provide your phrase");
+    io::stdin().read_line(&mut read_phrase).unwrap();
+
+    let new_phrase = cipher(read_phrase, alphabet, offset.try_into().unwrap());
+    println!("Cipher: {:?}", new_phrase)
+}
+
+fn cipher(phrase: String, alphabet: Vec<char>, offset: usize) -> String {
+    let mut new_phrase = String::new();
+    for c in phrase.trim().chars() {
+        if c == ' ' {
+            new_phrase.push(' ')
+        } else {
+            let index = alphabet.iter().position(|&r| r == c).unwrap();
+            let at_index = index + offset;
+            let current = alphabet[at_index];
+            new_phrase.push(current)
+        }
+    }
+    new_phrase
+}
